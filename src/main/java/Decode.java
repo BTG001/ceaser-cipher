fdfb990f826fad0e239c356c9ca2dfe533b6c0d7
BTG001 <brunokipkorir2@gmail.com>
2020-06-30 15:30:07 +0300

    Test passes after declaring the getPhrase() method and passing logic inside it

--- src/main/java/Decode.java
+++ src/main/java/Decode.java
@@ -1,11 +1,16 @@
 public class Decode {
     private int key;
+    private String phrase;
 
     public Decode(int key, String phrase) {
         this.key = key;
+        this.phrase = phrase;
     }
 
     public int getKey() {
         return this.key;
+    }
+    public String getPhrase() {
+        return this.phrase;
     }
 }
