a4e8ebce8c2549df84987287610ecc090e6212df
BTG001 <brunokipkorir2@gmail.com>
2020-06-30 15:03:47 +0300

    Test passes to ascertain testDecode is an instance of Decode

--- src/test/java/DecodeTest.java
+++ src/test/java/DecodeTest.java
@@ -3,8 +3,8 @@ import static org.junit.Assert.*;
 
 public class DecodeTest {
     @Test
-    public void newDecode_instantiatesCorrectly() throws Exception {
-        Decode newDecode = new Decode(2, "nobu");
-        assertEquals(true, newDecode instanceof Decode);
+    public void newDecode_instantiatesCorrectly() {
+        Decode testDecode = new Decode(2, "nobu");
+        assertEquals(true, testDecode instanceof Decode);
     }
 }
