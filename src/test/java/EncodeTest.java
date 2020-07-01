0407f1ff16f9abf0b5e03ce5aef03417477337a6
BTG001 <brunokipkorir2@gmail.com>
2020-07-01 08:22:43 +0300

    Test to ascertain testEncode is an instanceOf Encode

--- src/test/java/EncodeTest.java
+++ src/test/java/EncodeTest.java
@@ -1,2 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class EncodeTest {
+    @Test
+    public void newEncode_instantiatesCorrectly() {
+        Encode testEncode = new Encode(2, "bonu");
+        assertEquals(true, testEncode instanceof Encode);
+    }
 }
